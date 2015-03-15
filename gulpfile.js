e950593128da7d0e55134f7c41ec4427364c2f02
Your Name <jackiegleason@gmail.com>
2015-03-14 22:15:29 -0400

    Changing back because I don't think html2js supports ng2 yet :-P

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var shell = require('gulp-shell');
 var open = require('gulp-open');
-var concat = require('gulp-concat');
-var html2js = require('gulp-html2js');
 var port = 3456;
 
 // git clone Angular 2 quickstart
@@ -26,15 +24,5 @@ gulp.task('open', function(){
   gulp.src('./index.html')
   .pipe(open('', options));
 });
-
-gulp.task('scripts', function() {
-  gulp.src('fixtures/*.html')
-    .pipe(html2js({
-      outputModuleName: 'template-test',
-      useStrict: true
-    }))
-    .pipe(concat('template.js'))
-    .pipe(gulp.dest('./'))
-})
 
 gulp.task('serve', ['connect', 'open']);
